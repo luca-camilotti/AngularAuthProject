23ce94642a7963f19efa11a6836e195bf6b852a4
luca.camilotti <luca.camilotti@gmail.com>
2022-11-11 19:11:56 +0100

    logout added

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { EventData } from 'src/app/models/event-data.model';
+import { EventBusService } from 'src/app/services/event-bus.service';
+import { MessageService } from 'src/app/services/message.service';
+import { TokenStorageService } from 'src/app/services/token-storage.service';
 import { environment } from 'src/environments/environment';
 
 @Component({
@@ -9,9 +13,17 @@ import { environment } from 'src/environments/environment';
 export class HeaderComponent implements OnInit {
 
   private readonly appLogin: string = environment.appLogin;
-  constructor() { }
+  constructor(public eventBusService: EventBusService, public tokenService: TokenStorageService,
+    public UImessage: MessageService) { }
 
   ngOnInit(): void {
+  }
+
+  logOut(): void {
+    this.eventBusService.emit(new EventData('logout', null));  // send logout signal
+    this.tokenService.signOut();    // sign out
+    console.log("Logout!");
+    this.UImessage.sendMessage("Logout successful");
   }
 
 }
